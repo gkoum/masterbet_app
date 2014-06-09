401cda5d851f75b64bd144e9c51dcce547e48a10
gkoum <koumoutsosgian@yahoo.gr>
2014-06-09 15:27:53 +0300

    gemfile with rspec-capybara

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MasterbetApp::Application.config.secret_key_base = 'fe0e2c0b48c680206a8103c4b0afca0729eac947946e288216ef716eb30d32a65c9f3f45e0f9fededf086d08f85337f37b8508fd729766c6430b01a690fb0c25'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MasterbetApp::Application.config.session_store :cookie_store, key: '_masterbet_app_session'
